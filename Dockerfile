814cd7cc1dc37c70c369059948c8cdf9f8d8c558
Ryan Cahill <ryan.cahill444@gmail.com>
2020-08-12 15:42:19 -0400

    testing composite action

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:12-alpine
 
-RUN apk add python3
+RUN apk add python3 docker
 
-RUN npm install -g @architect-io/cli && architect version
+RUN npm install -g @architect-io/cli
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ cat README.md
 
 architect login -u $INPUT_ARCHITECT_USERNAME -p $INPUT_ARCHITECT_PASSWORD
 # TODO: echo steps
-architect register --tag $INPUT_ARCHITECT_ENVIRONMENT_NAME ./architect.yml
-
+architect register --tag $INPUT_ARCHITECT_ENVIRONMENT_NAME ./architect.yml # TODO: install docker
+# TODO: keep using same name as tag and environment
 architect environment:create $INPUT_ARCHITECT_ENVIRONMENT_NAME -a $INPUT_ARCHITECT_ACCOUNT --platform $INPUT_ARCHITECT_PLATFORM
 
 echo $INPUT_ARCHITECT_ENVIRONMENT_CONFIG > environment.yml
